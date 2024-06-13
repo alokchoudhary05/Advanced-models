1226ba9a9f5c3b2414aa222321d55acfe21892e5
alokchoudhary05 <alokkumarchoudhary03@gmail.com>
2024-06-14 00:55:41 +0530

    app

---
+++ app.py
@@ -0,0 +1,108 @@
+from flask import Flask, render_template, request
+import pickle
+import warnings
+from dashboard import dashboard_bp
+
+
+app = Flask(__name__)
+app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
+
+
+
+# Load models
+pregnancy_model = pickle.load(open("models/Pregnancy.sav", 'rb'))
+heart_model = pickle.load(open("models/Heart.sav", 'rb'))
+diabetic_model = pickle.load(open("models/Diabetes.sav", 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+@app.route('/pregnancy', methods=['GET', 'POST'])
+def pregnancy():
+    risk_level = None
+    color = None
+    if request.method == 'POST':
+        age = request.form['age']
+        diastolicBP = request.form['diastolicBP']
+        BS = request.form['BS']
+        bodyTemp = request.form['bodyTemp']
+        heartRate = request.form['heartRate']
+        
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            predicted_risk = pregnancy_model.predict([[age, diastolicBP, BS, bodyTemp, heartRate]])
+        
+        if predicted_risk[0] == 0:
+            risk_level = "Low Risk"
+            color = "green"
+        elif predicted_risk[0] == 1:
+            risk_level = "Medium Risk"
+            color = "orange"
+        else:
+            risk_level = "High Risk"
+            color = "red"
+    
+    return render_template('pregnancy.html', risk_level=risk_level, color=color)
+
+@app.route('/heart', methods=['GET', 'POST'])
+def heart():
+    prediction_text = None
+    if request.method == 'POST':
+        data = [
+            request.form['age'],
+            request.form['sex'],
+            request.form['cp'],
+            request.form['trestbps'],
+            request.form['chol'],
+            request.form['fbs'],
+            request.form['restecg'],
+            request.form['thalach'],
+            request.form['exang'],
+            request.form['oldpeak'],
+            request.form['slope'],
+            request.form['ca'],
+            request.form['thal']
+        ]
+
+        input_data = [data]
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            heart_prediction = heart_model.predict(input_data)
+        
+        if heart_prediction[0] == 1:
+            prediction_text = 'The person is having heart disease'
+        else:
+            prediction_text = 'The person does not have any heart disease'
+    
+    return render_template('heart.html', prediction_text=prediction_text)
+
+@app.route('/diabetes', methods=['GET', 'POST'])
+def diabetes():
+    prediction_text = None
+    if request.method == 'POST':
+        data = [
+            request.form['Pregnancies'],
+            request.form['Glucose'],
+            request.form['BloodPressure'],
+            request.form['SkinThickness'],
+            request.form['Insulin'],
+            request.form['BMI'],
+            request.form['DiabetesPedigreeFunction'],
+            request.form['Age']
+        ]
+
+        input_data = [data]
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            prediction = diabetic_model.predict(input_data)
+
+        if prediction[0] == 1:
+            prediction_text = 'The person is diabetic'
+        else:
+            prediction_text = 'The person is not diabetic'
+    
+    return render_template('diabetes.html', prediction_text=prediction_text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
